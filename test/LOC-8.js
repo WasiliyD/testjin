8adb60661df891b0935fd097b5dab15286fa6100
Wasiliy <testun.db@gmail.com>
2024-01-18 02:57:29 +0500

    sdfsdfdsfsdfsdf

--- test/LOC-8.js
+++ test/LOC-8.js
@@ -5,6 +5,7 @@ const { Builder, By, until, error } = require("selenium-webdriver");
 describe.only("Главная страница", async function () {
   it("LOC-8 Количество авто не равно 0", async function () {
     openPage("https://ya.ru/");
+    await driver.sleep(150000);
     await driver.findElement(By.css("tr-car-new-card"));
     await driver.sleep(5000);
   });
